9fd9e2bac9e9d9f6eb829354662299a2101427bd
Manuel Torres <manueltorresvesga@gmail.com>
2021-03-17 13:49:28 -0500

    Move Textarea to Keyboard component

--- src/components/Divtext.js
+++ src/components/Divtext.js
@@ -40,9 +40,9 @@ const Divtext = () => (
         overflow="visible"
         border={2}
       > 
-        <Box borderRadius="10px" boxShadow="dark-lg" bg="#E6E6E6" wordBreak="break-word" minHeight="128px">
-          <Textarea borderRadius="10px" h="100%" placeholder="Type something..." />
-        </Box>
+        {/* <Box borderRadius="10px" boxShadow="dark-lg" bg="#E6E6E6" wordBreak="break-word" minHeight="128px"> */}
+        {/*   <Textarea boderRadius="10px" h="100%" placeholder="Type something..." /> */}
+        {/* </Box> */}
       </SimpleGrid>
     </Container>
 )
